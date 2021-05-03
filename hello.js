e02e3dd7fb4ec5dbf5522a63bed6b3a383854515
sreavis <>
2021-05-03 19:53:55 +0000

    Tried again and it shows "Hello world!"

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ function hello() {
 	// TODO: change this string so that your program prints "Hello world!" 
 	// when you hit the replit run button
   //I know return works like config.log ()
-	return "Hello World!";
+	return ("Hello world!");
 }
  
 module.exports = hello;
